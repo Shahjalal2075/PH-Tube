1a93cb9e1f9114ac61734326f3536c7cf9cd1883
Shahjalal2075 <shahjalal2075@gmail.com>
2023-09-02 02:28:05 +0600

    fornt end done without responsive

--- js/tube.js
+++ js/tube.js
@@ -1,1 +1,15 @@
-console.log("OK");
+const loadUserInfo = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await res.json();
+    const info = data.data;
+    console.log(info);
+    displayAll(info);
+}
+
+const displayAll = info => {
+    info.forEach(det => {
+        console.log(det);
+    });
+}
+
+loadUserInfo();
