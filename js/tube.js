d96aee6c98d2f383ac3ac5c8e15dff8736712192
Shahjalal2075 <shahjalal2075@gmail.com>
2023-09-02 02:52:59 +0600

    backend details add

--- js/tube.js
+++ js/tube.js
@@ -7,8 +7,29 @@ const loadUserInfo = async() =>{
 }
 
 const displayAll = info => {
+    const cardContainer = document.getElementById('card-container');
     info.forEach(det => {
         console.log(det);
+
+        const cardDet = document.createElement('div');
+        cardDet.classList = `card bg-site-white`;
+        cardDet.innerHTML = 
+        `
+        <figure class="rounded-lg h-44">
+            <img src="${det.thumbnail}" alt="" />
+        </figure>
+        <div class="flex my-4 gap-4 px-1">
+            <div class="">
+                <img class="mask mask-circle w-10" src="${det.authors[0].profile_picture}" />
+            </div>
+            <div class="">
+                <h2 class="text-site-black font-bold text-sm">${det.title}</h2>
+                <p class="text-site-black font-normal text-xs py-1">${det.authors[0].profile_name}</p>
+                <p class="text-site-black font-normal text-xs">${det.others.views}</p>
+            </div>
+        </div>
+        `;
+        cardContainer.appendChild(cardDet);
     });
 }
 
